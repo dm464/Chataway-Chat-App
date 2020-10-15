7ce21b33e5454a37be9dd38a10473d1f5f4b0b4c
EC2 Default User <ec2-user@ip-172-31-47-106.ec2.internal>
2020-10-15 08:37:34 +0000

    Add files to create the app skeleton

---
+++ models.py
@@ -0,0 +1,17 @@
+# models.py
+import flask_sqlalchemy
+from app import db
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(50))
+    message = db.Column(db.String(160))
+    
+    def __init__(self, u, m):
+        self.user = u
+        self.message = m
+        
+    def __repr__(self):
+        return '<Username: %s\tMessage: %s>' % (self.user, self.message)
+
---
+++ scripts/Content.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+
+import { Button } from './Button';
+import { Socket } from './Socket';
+
+export function Content() {
+    const [addresses, setAddresses] = React.useState([]);
+    
+    function getNewAddresses() {
+        React.useEffect(() => {
+            Socket.on('addresses received', updateAddresses);
+            return () => {
+                Socket.off('addresses received', updateAddresses);
+            }
+        });
+    }
+    
+    function updateAddresses(data) {
+        console.log("Received addresses from server: " + data['allAddresses']);
+        setAddresses(data['allAddresses']);
+    }
+    
+    getNewAddresses();
+
+    return (
+        <div>
+            <h1>USPS Addresses!</h1>
+                <ol>
+                    {
+                    // TODO -- display all addresses
+                        addresses.map(
+                            (address, index) => <li key={index}>{address}</li>)
+                    }
+                </ol>
+            <Button />
+        </div>
+    );
+}
