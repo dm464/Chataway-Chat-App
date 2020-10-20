ccdb8153763be15a94567731a33dd6fe19991f89
EC2 Default User <ec2-user@ip-172-31-47-106.ec2.internal>
2020-10-20 13:33:14 +0000

    Fix bot.py to properly display error message

--- bot.py
+++ bot.py
@@ -18,11 +18,7 @@ IPSTACK_KEY=os.environ["IPSTACK_KEY"]
 def is_bot_command(message):
     message_arr = message.split(" ")
     if message_arr[0]=="!!":
-        commands = [ABOUT, HELP, LOCATION, TIME, DATE, FUNSTRANSLATE]
-        if message_arr[1] in commands:
-            return True
-        else:
-            return False
+        return True
     else:
         return False
 
@@ -44,7 +40,7 @@ def bot_reply(message):
     elif command == FUNSTRANSLATE:
         return funtranslate_command(message)
     else:
-        return "Uh oh! That's not a valid command. Type \"!! Help\" for guidance"
+        return "Uh oh! \'{}\' is not a valid command. Type \'!! help\' for guidance.".format(message)
 
 def about_command():
     return "How art thou? T'is I, none other than Sir Robot. I am hither to assist thee."
@@ -56,7 +52,7 @@ def help_command():
     ret_str += "!! location - gives current location\n"
     ret_str += "!! time - gives current local time\n"
     ret_str += "!! date - give current date\n"
-    ret_str += "!! funtranslate <message> - I'll translate the message into old English\n"
+    ret_str += "!! funtranslate <message> - I'll translate the message into Shakespearean English\n"
     return ret_str;
 
 def location_command():
@@ -77,6 +73,7 @@ def date_command():
     return "Today is " + today
 
 def funtranslate_command(message):
+    message = message[len("!! {} ".format(FUNSTRANSLATE)):]
     url = "https://api.funtranslations.com/translate/shakespeare.json?text={}".format(message)
     json_body = requests.get(url).json()
     translated_message = json_body["contents"]["translated"]
